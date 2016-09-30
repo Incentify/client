b6f7fa07eb40b326b0f774dbdd6c647ffdb3fea6
Ryan Wilson <comrww@gmail.com>
2016-09-30 09:30:21 -0700

    today

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,5 +1,34 @@
 <template>
-    <h1>Profile!</h1>
+	<div class="">
+	<br>
+	    <div class="card">
+	    	<div class="card-header">
+	        	<h4 class="card-title"></h4>
+	        	<h6 class="card-meta"></h6>
+	    </div>
+	    <div class="card-body">
+			<div class="form-group">
+			    <br>
+			    <form action="" class="form-horizontal">
+			    	<label class="form-label super-mario" for="">name</label>
+			    	<input class="form-label super-mario"type="text">
+			    	<br>
+			    	<label class="form-label super-mario" for="">username</label>
+			    	<input class="form-label super-mario"type="text">
+			    	<label class="form-label super-mario"for="">password</label>
+			    	<input class="form-label super-mario"type="text">
+					<br>
+					<label class="form-label super-mario"for="">age</label>
+			    	<input class="form-label super-mario"type="text">
+			    	<br>
+
+			    </form>    
+			</div>
+		    <div class="card-footer">
+		        <button class="btn btn-primary btn-on-profile red-nav">Edit</button>
+	    	</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -14,6 +43,21 @@ export default {
   },
 };
 </script>
-
+	
 <style lang="css" scoped>
+	.super-mario {
+		font-size: 40px !important;
+		line-height: 200%;
+	}
+
+	.btn-on-profile {
+	    padding: 10px !important;
+	    font-size: 20px !important;
+	    text-transform: uppercase !important;
+	    height: 100px !important;
+	    width: 200px !important;
+  	}
+	.some-padding-profile {
+		padding-top: 50px;
+	}
 </style>
--- src/components/top-branding.vue
+++ src/components/top-branding.vue
@@ -1,4 +1,9 @@
 <template>
+	<div class="modal-footer">
+	    <div class="btn-group btn-group-block">
+	          <a class="btn top-padding red-nav" href="#" v-link="{path:'dashboard'}"><img src="../assets/incentify-logo-rectangle.png"></a>
+	    </div>
+	  </div>
 </template>
 
 <script>
@@ -15,4 +20,10 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.top-padding {
+    padding: 10px !important;
+    font-size: 20px !important;
+    text-transform: uppercase !important;
+    height: 120px !important;
+  }
 </style>
