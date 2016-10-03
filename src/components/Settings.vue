7f03b9a78358ee0d4299a3614af4569a7d119ac1
wishinghand <wishinghand@gmail.com>
2016-10-03 16:48:32 -0700

    added missing word

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -39,7 +39,7 @@
       <p>Redistribute content from Incentify (unless content is specifically made for redistribution).</p>
 
       <h3>User Comments</h3>
-      <ol>
+        <ol>
         <li>This Agreement shall begin on the date hereof.</li>
         <li>Certain parts of this website offer the opportunity for users to post and exchange opinions, information, material and data ('Comments') in areas of the website. Incentify does not screen, edit, publish or review Comments prior to their appearance on the website and Comments do not reflect the views or opinions of Incentify, its agents or affiliates. Comments reflect the view and opinion of the person who posts such view or opinion. To the extent permitted by applicable laws Incentify shall not be responsible or liable for the Comments or for any loss cost, liability, damages or expenses caused and or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.</li>
         <li>Incentify reserves the right to monitor all Comments and to remove any Comments which it considers in its absolute discretion to be inappropriate, offensive or otherwise in breach of these Terms and Conditions.</li>
@@ -127,7 +127,7 @@
     <div class="card-content">
       <div class="content">
         <div v-on:click = "showMission = !showMission" v-show = "showMission"><strong>Mission Statement</strong><i class="fa fa-plus is-pulled-right" style="font-size: 60px;" aria-hidden="true"></i></div>
-         <div v-on:click = "showMission = !showMission" v-show = "!showMission"><strong>Mission</strong><i class="fa fa-minus is-pulled-right" style="font-size: 60px;" aria-hidden="true"></i></div>
+         <div v-on:click = "showMission = !showMission" v-show = "!showMission"><strong>Mission Statement</strong><i class="fa fa-minus is-pulled-right" style="font-size: 60px;" aria-hidden="true"></i></div>
         <div v-show = "!showMission">
         <p>Our goal is to give people the motivation they need to be truly AWESOME!!!!!!!</p>
         </div>
