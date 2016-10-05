6f5ef614560828156b4dc05d815d98412fb2323b
wishinghand <wishinghand@gmail.com>
2016-10-05 11:33:05 -0700

    capitalizing navbar component

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,20 @@
     <!-- route outlet -->
     <top-branding></top-branding>
     <router-view class="wrapper"></router-view>
-    <navbar class="navbar-one"></navbar>
+    <Navbar class="Navbar-one"></Navbar>
   </div>
 </template>
 
 <script>
-  import navbar from './components/navbar';
+  import Navbar from './components/Navbar';
   import topBranding from './components/top-branding';
 
-  // require('spectre.css/dist/spectre.css');
-  // require('bulma/css/bulma.css');
   require('bulma/bulma.sass');
   require('font-awesome/css/font-awesome.css');
 
   export default {
     components: {
-      navbar,
+      Navbar,
       topBranding,
     },
     replace: false,
@@ -34,7 +32,7 @@
     background-color: ghostwhite !important;
   } */
 
-  .navbar-one {
+  .Navbar-one {
     position: fixed;
     bottom: 0px;
     width: 100%;
