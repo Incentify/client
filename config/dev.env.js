337e229e94440ff721a8e8b0f938a12a03485386
wishinghand <wishinghand@gmail.com>
2016-10-25 10:59:00 -0700

    adding DEBUG keys to environment files

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,6 +2,7 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
+  DEBUG_MODE: true,
   NODE_ENV: '"development"',
   API_URL: 'http://localhost:3000/'
 })
